810f1c99f50949a1d3af9ff6a9067edc0b19bb79
MuratGuneyKemal <muratguneykemal@gmail.com>
2023-02-10 00:53:18 +0300

    final commit

--- profiles/permissions.py
+++ profiles/permissions.py
@@ -9,3 +9,13 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         
         return obj.id == request.user.id
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Update status permission"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check permission"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        
+        return obj.user_profile.id == request.user.id
